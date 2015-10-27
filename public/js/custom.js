6b94655b59fbabf95fd023c45fef990409c26cab
liukanghui <liukanghui@liukanghuideMacBook-Air.local>
2015-10-26 22:30:55 -0700

    finish everything

--- public/js/custom.js
+++ public/js/custom.js
@@ -42,9 +42,9 @@
             $(function () {
                 $.vegas('slideshow', {
                     backgrounds: [
-                    { src: 'images/background/background1.png', fade: 1000, delay: 50000 }, 
-                     { src: 'images/background/background2.png', fade: 1000, delay: 50000}, 
-                     { src: 'images/background/background3.png', fade: 1000, delay: 50000 }
+                    { src: 'images/background/background2.png', fade: 1000, delay: 50000 }, 
+                     { src: 'images/background/background3.png', fade: 1000, delay: 50000}, 
+                     { src: 'images/background/background1.png', fade: 1000, delay: 50000 }
                                          
                     ]
                 })
@@ -112,23 +112,20 @@
     $(document).ready(function () {
         mainApp.main_fun();
 
-        $('.cntl').cntl({
+//         $('.cntl').cntl({
 
-        // The amount of "scroll padding" to allow 
-        // The more, the later the state will be revealed
-        revealbefore: 300,
+//         // The amount of "scroll padding" to allow 
+//         // The more, the later the state will be revealed
+//         revealbefore: 300,
 
-        // The animate class
-        // This class should have animation rules in css
-        anim_class: 'cntl-animate',
+//         // The animate class
+//         // This class should have animation rules in css
+//         anim_class: 'cntl-animate',
 
-        // A callback once the state has been revealed
-        onreveal: null
+//         // A callback once the state has been revealed
+//         onreveal: null
 
-});
-
-
-
+// });
 
 
 
